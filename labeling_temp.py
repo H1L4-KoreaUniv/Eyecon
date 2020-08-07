e207a9ce6df8bf5624b2683f70afeaaaba19ebdf
Jiwon <zzwon1212@gmail.com>
2020-08-07 20:13:24 +0900

    temp

---
+++ labeling_temp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import json
+from collections import OrderedDict
+
+path_dir = 'C:/Users/JIWON/Downloads/temp'
+file_list = os.listdir(path_dir)
+
+data = []
+for i in file_list:
+    file_data = OrderedDict()
+    file_data['file_name'] = i
+    file_data['label'] = i[0]
+    data.append(file_data)
+data
+data_dict = {}
+data_dict['data'] = data
+data_dict
+
+# print(json.dumps(data_dict, ensure_ascii=False, indent='\t'))
+
+with open('C:/Users/JIWON/Downloads/temp/temp.json', 'w', encoding='utf-8') as make_file:
+    json.dump(data_dict, make_file, ensure_ascii=False, indent='\t')
+
