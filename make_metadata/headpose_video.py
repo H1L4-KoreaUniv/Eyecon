52d85925dde83791b91fa80162149fa4b5a8b4cd
Jiwon <zzwon1212@gmail.com>
2020-08-17 15:59:53 +0900

    modify make_metadata files
    Signed-off-by: Jiwon <zzwon1212@gmail.com>

--- make_metadata/headpose_video.py
+++ make_metadata/headpose_video.py
@@ -24,11 +24,15 @@ class Headpose_video():
         y1 = int(bbox[1]) 
         y2 = int(bbox[3])
         # 얼굴 사진 따로 저장할 하위 폴더명
-        facelmdir = '/Users/hong-yujin/Downloads/input_img/facelm_img/'
+        facelmdir = 'C:/Users/JIWON/workspace_python/Korea_Univ/Project/Data/Image/ver3/facelm_img/'
         # 전체 경로 포함한 저장 파일명
         facelmname = facelmdir + self.imgfile.split('/')[-1] + f'_frame{self.cnt}_facelm.jpg'
         # facelmname = self.imgfile.split('/')[0] + '/' + facelmdir + self.imgfile.split('/')[-1] + f'frame{self.cnt}_facelm.jpg'
-        cv2.imwrite(facelmname, cv2.resize(original[ y1:y2, x1:x1+y2-y1], dsize=(300, 300), interpolation=cv2.INTER_AREA))
+        try:
+            cv2.imwrite(facelmname, cv2.resize(original[ y1:y2, x1:x1+y2-y1], dsize=(300, 300), interpolation=cv2.INTER_AREA))
+        except cv2.error as e:
+            print('invalid frame: ', self.cnt)
+            return None
         return facelmname
 
     def run_video(self):
@@ -50,25 +54,33 @@ class Headpose_video():
                 break
             else:
                 original = frame.copy()
-                eyelm = detect_eye(original)
+                try:
+                    eyelm = detect_eye(original)
+                except cv2.error as e:
+                    print('detect_eye(original) error: ', eyelmname)
+                    continue
                 frame, angles, bbox = hpd.process_image(frame)
                 
                 if bbox is None:
                     print("pass bbox")
                     continue
                 else:
-                    if(int(cap.get(1)) % 30 == 0):  #fps에 따라 다르게
-                        eyelmdir = '/Users/hong-yujin/Downloads/input_img/eyelm_img/'
+                    if(int(cap.get(1)) % 16 == 0):  #fps에 따라 다르게
+                        eyelmdir = 'C:/Users/JIWON/workspace_python/Korea_Univ/Project/Data/Image/ver3/eyelm_img/'
                         eyelmname = eyelmdir + self.imgfile.split('/')[-1] + f'_frame{self.cnt}_eyelm'
                         if(hpd.get_eye(image=original, path=eyelmname) is None):
                             print("pass get eye")
                             continue
-                        facelmname.append(self.get_land_img(bbox,original))
+                        tmp = self.get_land_img(bbox,original)
+                        if (tmp is not None):
+                            facelmname.append(tmp)
+                        else:
+                            continue
                         ang.append(angles)
                         box.append(bbox)
                         eyelmlist.append(eyelm)
                         # 눈 사진 따로 저장할 하위 폴더명
-                        eyelmdir = '/Users/hong-yujin/Downloads/input_img/eyelm_img/'
+                        eyelmdir = 'C:/Users/JIWON/workspace_python/Korea_Univ/Project/Data/Image/ver3/eyelm_img/'
                         # 전체 경로 포함
                         
                         #eyelmname = self.imgfile.split('/')[0] + '/' + eyelmdir + self.imgfile.split('/')[-1] + f'frame{self.cnt}_eyelm'  # 눈 부분만 저장
@@ -76,7 +88,7 @@ class Headpose_video():
                         #print(angles) #angle
                         #print(bbox)
                         temp = self.imgfile.split('/')[-1]
-                        imgname = '/Users/hong-yujin/Downloads/input_img/raw_img/'+f'{temp}_frame{self.cnt}.jpg'  # 캡쳐한 raw 이미지 저장
+                        imgname = 'C:/Users/JIWON/workspace_python/Korea_Univ/Project/Data/Image/ver3/raw_img/'+f'{temp}_frame{self.cnt}.jpg'  # 캡쳐한 raw 이미지 저장
                         cv2.imwrite(imgname, original)
                         print(f'Saved frame{self.cnt}.jpg') 
                         
