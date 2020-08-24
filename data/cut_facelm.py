7fa6f588b93325cacd6add086e47c961721cb164
dhelee <46590618+dhelee@users.noreply.github.com>
2020-08-24 20:32:08 +0900

    Update cut_facelm.py

--- data/cut_facelm.py
+++ data/cut_facelm.py
@@ -1,9 +1,13 @@
 import cv2
 
-
-def get_facelm_img(bbox, original):
-    x1 = int(bbox[0])
-    y1 = int(bbox[1])
-    y2 = int(bbox[3])
+# face boundarybox 좌표값과 이미지를 인자로 받아 얼굴 부분만 자른 이미지를 리턴하는 함수
+def get_facelm_img(rect, original):
+    
+    x1 = int(rect[0])
+    y1 = int(rect[1])
+    y2 = int(rect[3])
+    
+    # resize by 300*300
     facelm_img = cv2.resize(original[y1:y2, x1:x1 + y2 - y1], dsize=(300, 300), interpolation=cv2.INTER_AREA)
+    
     return facelm_img
